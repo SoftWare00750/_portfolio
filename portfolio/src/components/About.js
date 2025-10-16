e4a78e24ef3e99f0076c0cdad738e2307caa4c70
SoftWare00750 <tunesbaby7175@gmail.com>
2025-10-16 06:54:21 +0000

    12th commit

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -8,10 +8,10 @@ export default function About() {
                     <h2 className="section-title">About</h2>
                             <div className="about-grid">
                                       <div>
-                                                  <p>
-                                              I’m a Full Stack Developer experienced in building responsive websites and web applications. I create modern interfaces and develop scalable back-end systems. <p>I’m passionate about clean code, performance, and delivering reliable, user-focused solutions.</p>                 
+                                       
+                                             <p>I’m a Full Stack Developer experienced in building responsive websites and web applications.</p> <p className="about2">I create modern interfaces and develop scalable back-end systems.</p> <p className="about2">I’m passionate about clean code, performance, and delivering reliable, user-focused solutions.</p>                 
                     
-                                                                                                                      </p>
+                                                                                                                     
                                                                                                                                   <ul>
                                                                                                                                                 <li>Web Interfaces with <span className="skills1">Html</span>, <span className="skills1">CSS</span>, <span className="skills1">Tailwaind CSS</span> and <span className="skills1">Javascript</span> </li>
                                                                                                                                                               <li>Back-end Systems with <span className="skills1">Laravel</span>, <span className="skills1">Node.js</span>, <span className="skills1">Django</span>, <span className="skills1">Spring</span> Frameworks, <span className="skills1">MySQL</span> and <span className="skills1">MongoDB</span> </li>
