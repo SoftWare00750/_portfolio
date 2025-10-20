e89818d10647b1762ad2bb7ab5967b7e2059ec32
SoftWare00750 <tunesbaby7175@gmail.com>
2025-10-20 17:40:57 +0100

    19th commit

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
       <footer className="footer">
             <div className="container">
-                    <div>© {new Date().getFullYear()} Oluwadamilola Otunla</div>
+                  
                             <div className="socials">
                                       <a href="https://github.com/SoftWare00750?tab=repositories" aria-label="github">Github</a>
-                                                <a href="www.linkedin.com/in/oluwadamilola-o-778047222" aria-label="linkedin">LinkedIn</a>
+                                               
                                                         </div>
                                                               </div>
                                                                   </footer>
