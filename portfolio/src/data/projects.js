4d3fc305b9831f4d7d173ffc58a1d3bd6095d639
Coding <150249195+Dammy-wise@users.noreply.github.com>
2025-10-31 07:04:45 +0100

    128a

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -1,6 +1,6 @@
 import mygarage from "./assets/mygarage.png" ;
 import nike  from "./assets/nike.png" ;
-import ecommerce from "./assets/ecomerce.png" ;
+import ecommerce from "/data/ecomerce.png" ;
 
 const projects = [
       {
