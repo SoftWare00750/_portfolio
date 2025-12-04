7b2cf8e6c6322f9ca23c89d3b15b3463daf695c3
SoftWare00750 <tunesbaby7175@gmail.com>
2025-12-04 12:40:21 +0100

    commit 2

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -12,9 +12,9 @@ export default function Hero() {
           </h1>
           <span className="hero-sub">
           <p className="hero-sub-right-fixed">
-              Frontend Web Developer 
+              Frontend Developer 
               </p>
-             <p className="hero-sub-right2"> <span>I handle the development, deployment</span> <br /> <span>and maintenance of your Websites UI, start to finish.</span> </p>
+             <p className="hero-sub-right2"> <span>I handle the development, deployment</span> <br /> <span>and maintenance of your Website and App UI, start to finish.</span> </p>
             </span>
              <img src="/assets/coder.png" alt="coder" className="hero-image" />
           
