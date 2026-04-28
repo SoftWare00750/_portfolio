53e8de9ac674549354ffb249b00fad5fd478bc81
Coding <150249195+Dammy-wise@users.noreply.github.com>
2026-04-28 17:09:07 +0100

    commit85

---
+++ portfolio/src/data/skills.js
@@ -0,0 +1,41 @@
+export const languages = {
+  x: 0,           // SVG rect x offset
+  width: 500,      // SVG rect width
+  height: 175,     // SVG rect height
+  stats: [
+    { name: "JavaScript", value: 90 },
+    { name: "Python",     value: 80 },
+    { name: "Java",       value: 75 },
+    { name: "C",          value: 65 },
+    { name: "PHP",        value: 55 },
+    { name: "HTML/CSS",   value: 85 },
+    { name: "Kotlin",     value: 50 },
+  ],
+};
+
+export const frameworks = {  x: 0,           // SVG rect x offset
+  width: 500,      // SVG rect width
+  height: 175,     // SVG rect height
+  stats: [
+    { name: "React", value: 90 },
+    { name: "React-nstive",     value: 80 },
+    { name: "Angular",       value: 75 },
+    { name: "Vue",          value: 65 },
+    { name: "Tailwind CSS",   value: 55 },
+    { name: "Sql",   value: 85 },
+    { name: "MongoDB",     value: 50 },
+  ],
+ };
+
+export const tools      = {  width: 500,      // SVG rect width
+  height: 175,     // SVG rect height
+  stats: [
+    { name: "Git", value: 90 },
+    { name: "Docker",     value: 80 },
+    { name: "Figma",       value: 75 },
+    { name: "Vue",          value: 65 },
+    { name: "Vercel",   value: 55 },
+    { name: "Sql",   value: 85 },
+    { name: "MongoDB",     value: 50 },
+    { name: "AWS cloud",     value: 50 },
+  ], };
