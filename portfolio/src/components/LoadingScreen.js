e94ac4b15d796a6f051474e036501ff11a764882
Steph Otunla <tunesbaby7175@gmail.com>
2026-08-05 03:38:02 +0100

    commit134

--- portfolio/src/components/LoadingScreen.js
+++ portfolio/src/components/LoadingScreen.js
@@ -108,7 +108,7 @@ const LoadingScreen = () => {
             color: textColor,
             fontSize: '2.4rem',
             fontWeight: 700,
-            fontFamily: 'Inter, system-ui, sans-serif',
+            fontFamily: "'Sprite Graffiti', Inter, system-ui, sans-serif",
             margin: 0,
             textAlign: 'center',
             minHeight: 56,
